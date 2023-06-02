97af1725d63263b84e83c0970dc54fb361467baf
ydeezly <ydglitch@gmail.com>
2023-06-02 14:27:51 +0000

    polish commands and handle arg errs

--- cmd/spydey.go
+++ cmd/spydey.go
@@ -5,6 +5,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"github.com/dglitxh/tyrenz/spydey"
 	"github.com/spf13/cobra"
 )
@@ -16,21 +17,27 @@ var spydeyCmd = &cobra.Command{
 	Short: "A powerful file system crawler.",
 	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// spydey.Find(args[0])
-		spydey.Crawl(allow)
+		if len(args) > 0 {
+			switch args[0] {
+				case "find": 
+					if err := checkArgs(args, "please add filename to search"); err == nil {
+						spydey.Find(args[1])
+					} else {
+						fmt.Println(err)
+					}
+				case "crawl":
+					spydey.Crawl(allow)
+				default:
+					fmt.Println(" Please add a valid action.")
+		}
+		} else {
+			fmt.Println("Please add an action \n  **actions include [crawl, find]")
+		}
 	},
 }
 
 func init() {
 	spydeyCmd.Flags().BoolVarP(&allow, "allow_hidden", "a", false, "crawl hidden files and dirs.")
 	rootCmd.AddCommand(spydeyCmd)
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// spydeyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// spydeyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	
 }
