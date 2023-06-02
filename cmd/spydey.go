af287c9432c9a782d82b9a59ff4e4eb7eb76b7ae
ydeezly <ydglitch@gmail.com>
2023-06-02 14:30:29 +0000

    add dir flag to cmd

--- cmd/spydey.go
+++ cmd/spydey.go
@@ -6,11 +6,13 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/dglitxh/tyrenz/spydey"
 	"github.com/spf13/cobra"
 )
 
 var allow bool
+var dir string
 // spydeyCmd represents the spydey command
 var spydeyCmd = &cobra.Command{
 	Use:   "spydey",
@@ -21,7 +23,7 @@ var spydeyCmd = &cobra.Command{
 			switch args[0] {
 				case "find": 
 					if err := checkArgs(args, "please add filename to search"); err == nil {
-						spydey.Find(args[1])
+						spydey.Find(args[1], dir)
 					} else {
 						fmt.Println(err)
 					}
@@ -38,6 +40,7 @@ var spydeyCmd = &cobra.Command{
 
 func init() {
 	spydeyCmd.Flags().BoolVarP(&allow, "allow_hidden", "a", false, "crawl hidden files and dirs.")
+	spydeyCmd.Flags().StringVarP(&dir, "directory", "d", ".", "add a root directory for operations.")
 	rootCmd.AddCommand(spydeyCmd)
 	
 }
