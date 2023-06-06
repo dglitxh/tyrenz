8220565763fde5bf22286c91806b5df8dc8cb2b8
ydeezly <ydglitch@gmail.com>
2023-06-06 16:54:38 +0000

    upd: ds

--- pomodoro/pomo.go
+++ pomodoro/pomo.go
@@ -12,3 +12,26 @@ const (
 	CatShortBreak = "ShortBreak"
 	CatLongBreak = "LongBreak"
 )
+
+type Actions interface {
+	Create() (string, error)
+	Update() (string, error)
+	GetById() (string, error)
+	Delete() (string, error)
+}
+
+const (
+	StateNotStarted = iota
+	StateRunning
+	StatePaused
+	StateDone
+	StateCancelled
+)
+
+type Config struct {
+	ID int64
+	StartTime time.Time
+	Duration time.Duration
+	Category string
+	State int
+}
