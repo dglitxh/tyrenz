d10444b16673ab8b0173d3d82a7e487430b971fa
ydeezly <ydglitch@gmail.com>
2023-06-16 18:27:15 +0000

    add: pomui

---
+++ pomodoro/pomui.go
@@ -0,0 +1,9 @@
+package  pomodoro
+
+import (
+	"context"
+	"github.com/mum4k/termdash/cell"
+	"github.com/mum4k/termdash/widgets/donut"
+	"github.com/mum4k/termdash/widgets/segmentdisplay"
+	"github.com/mum4k/termdash/widgets/text"
+)
