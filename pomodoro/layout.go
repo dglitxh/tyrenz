01afbda4fc83cd88c547f80ccc66820e3b8a460e
ydeezly <ydglitch@gmail.com>
2023-07-11 19:34:29 +0000

    qfix

--- pomodoro/layout.go
+++ pomodoro/layout.go
@@ -8,7 +8,7 @@ import (
 	"github.com/mum4k/termdash/terminal/terminalapi"
 )
 
-func newGrid(b *Buttons, w *widgets,
+func NewGrid(b *Buttons, w *widgets,
 t terminalapi.Terminal) (*container.Container, error) {
 	builder := grid.New()
 	builder.Add(
