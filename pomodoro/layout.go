d9e42318b130b82c7dc1f42c24123c7adffe2f4d
ydeezly <ydglitch@gmail.com>
2023-07-11 19:26:15 +0000

    upd: layout

--- pomodoro/layout.go
+++ pomodoro/layout.go
@@ -1,11 +1,56 @@
 package pomodoro
 
 import (
+	"github.com/mum4k/termdash/align"
 	"github.com/mum4k/termdash/container"
+	"github.com/mum4k/termdash/container/grid"
+	"github.com/mum4k/termdash/linestyle"
 	"github.com/mum4k/termdash/terminal/terminalapi"
 )
 
 func newGrid(b *Buttons, w *widgets,
 t terminalapi.Terminal) (*container.Container, error) {
-	
+	builder := grid.New()
+	builder.Add(
+		grid.RowHeightPerc(30, grid.ColWidthPercWithOpts(30,
+		[]container.Option{
+			container.Border(linestyle.Light),
+			container.BorderTitle("Press Q to Quit"),
+		},
+		grid.RowHeightPerc(80,
+		grid.Widget(w.donTimer)),
+		grid.RowHeightPercWithOpts(20,
+		[]container.Option{
+			container.AlignHorizontal(align.HorizontalCenter),
+		},
+		grid.Widget(w.txtTimer,
+			container.AlignHorizontal(align.HorizontalCenter),
+			container.AlignVertical(align.VerticalMiddle),
+			container.PaddingLeftPercent(49),
+			),
+			),
+		),
+		grid.ColWidthPerc(70,
+			grid.RowHeightPerc(80,
+			grid.Widget(w.disType, container.Border(linestyle.Light)),
+			),
+			grid.RowHeightPerc(20,
+			grid.Widget(w.txtInfo, container.Border(linestyle.Light)),
+			),
+			),
+			),
+			)
+		builder.Add(
+			grid.RowHeightPerc(10,
+				grid.ColWidthPerc(50,
+				grid.Widget(b.start),
+				),
+				grid.ColWidthPerc(50,
+				grid.Widget(b.pause),
+				),
+				),
+			)
+			builder.Add(
+				grid.RowHeightPerc(60),
+				)
 }
