ad25843a9c82917ddc237142c8712f20b12d8ee9
ydeezly <ydglitch@gmail.com>
2023-07-12 14:30:17 +0000

    upd: pomo app

---
+++ pomodoro/pomodoro.go
@@ -0,0 +1,25 @@
+package pomodoro
+
+import (
+	"context"
+	"image"
+	"time"
+	"github.com/mum4k/termdash"
+	"github.com/mum4k/termdash/terminal/tcell"
+	"github.com/mum4k/termdash/terminal/terminalapi"
+	"pragprog.com/rggo/interactiveTools/pomo/pomodoro"
+)
+
+
+type App struct {
+	ctx context.Context
+	controller *termdash.Controller
+	redrawCh chan bool
+	errorCh chan error
+	term *tcell.Terminal
+	size image.Point
+}
+
+func New(config *pomodoro.IntervalConfig) (*App, error) {
+
+}
