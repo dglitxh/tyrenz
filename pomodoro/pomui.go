2eebc0a659155db87278a2d7d08ff5f20f2911a7
ydeezly <ydglitch@gmail.com>
2023-07-30 19:01:30 +0000

    added default text.

--- pomodoro/pomui.go
+++ pomodoro/pomui.go
@@ -48,6 +48,7 @@ func (w *widgets) Update(timer []int, txtType, txtInfo, txtTimer string,
 
 func NewWidgets(ctx context.Context, errorCh chan<- error) (*widgets, error) {
 	w := &widgets{}
+	dftext :=  "Idle, Press start to initiate next event..."
 	var err error
 	w.updateDonTimer = make(chan []int)
 	w.updateTxtType = make(chan string)
@@ -63,12 +64,12 @@ func NewWidgets(ctx context.Context, errorCh chan<- error) (*widgets, error) {
 		helpers.Logger("Seg disp err, w.distype ui")
 		return nil, err
 	}
-	w.txtInfo, err = NewText(ctx, w.updateTxtInfo, errorCh)
+	w.txtInfo, err = NewText(ctx, dftext, w.updateTxtInfo, errorCh)
 	if err != nil {
 		helpers.Logger("new text error")
 		return nil, err
 	}
-	w.txtTimer, err = NewText(ctx, w.updateTxtTimer, errorCh)
+	w.txtTimer, err = NewText(ctx, "", w.updateTxtTimer, errorCh)
 	if err != nil {
 		helpers.Logger("new text error")
 		return nil, err
@@ -76,9 +77,10 @@ func NewWidgets(ctx context.Context, errorCh chan<- error) (*widgets, error) {
 	return w, nil
 }
 
-func NewText(ctx context.Context, updateText <-chan string,
+func NewText(ctx context.Context, dftext string, updateText <-chan string,
 	errorCh chan<- error) (*text.Text, error) {
 	txt, err := text.New()
+	txt.Write(dftext)
 	if err != nil {
 		return nil, err
 	}
@@ -99,6 +101,8 @@ func NewText(ctx context.Context, updateText <-chan string,
 func NewSegmentDisplay(ctx context.Context, updateText <-chan string,
 	errorCh chan<- error) (*segmentdisplay.SegmentDisplay, error) {
 	sd, err := segmentdisplay.New()
+	sd.Write([]*segmentdisplay.TextChunk{
+		segmentdisplay.NewChunk("Welcome"), })
 	if err != nil {
 		helpers.Logger("Seg Disp error")
 		return nil, err
