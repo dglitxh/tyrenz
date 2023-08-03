41a649211f2f37a6c52536993894c56e3a2a3264
ydeezly <ydglitch@gmail.com>
2023-08-03 22:38:08 +0000

    upd: gob

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -4,13 +4,15 @@ import (
 	"os/exec"
 )
 
-type Process struct {
+type Step struct {
 	name string
 	dir string
 	cmd string
 	msg string 
 	args []string
 }
+
+type Process []Step
 
 func Execute () {
 	exec.Command()
