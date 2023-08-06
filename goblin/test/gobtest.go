19c68a563257d4798c1f34e8aeb910766ac118e0
ydeezly <ydglitch@gmail.com>
2023-08-06 05:24:28 +0000

    upd: test

--- goblin/test/gobtest.go
+++ goblin/test/gobtest.go
@@ -1,13 +1,27 @@
 package gobliin_test
 
 import (
+	"fmt"
 	"testing"
+
 	"github.com/dglitxh/tyrenz/goblin"
 )
 
 func  TestAddStep (t *testing.T) {
-	t.Run("add step", func(t *testing.T) {
-		
-	})
+	proc := make(goblin.Process, 0)
+	step := goblin.Step{
+		Name: "make directory",
+		Dir: ".",
+		Msg: "create a new dir",
+		Cmd: "mkdir temp",
+		Args: make([]string, 0),
 
+	}
+	
+	t.Run(step.Name, func(t *testing.T) {
+		s := proc.NewStep(step)
+		if step.Name != s.Name {
+			fmt.Errorf()
+		}
+	})
 }
