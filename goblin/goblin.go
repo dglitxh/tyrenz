955b532a44a2022c6a6726e9524b7ce5df15831d
ydeezly <ydglitch@gmail.com>
2023-08-09 17:26:13 +0000

    add: log

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -2,8 +2,13 @@ package goblin
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+
+	"github.com/dglitxh/tyrenz/helpers"
 )
+
+var logfn *os.File = helpers.CreateLogFile(".goblogs.txt")
 
 type Step struct {
 	Name string
@@ -37,9 +42,9 @@ func (s Step) Execute () error{
 }
 
 func (p *Process) Run () error {
-	for _, v := range *p {
+	for i, v := range *p {
 		if err := v.Execute(); err != nil {
-			fmt.Printf("%v at %s", err, v.Name)
+			helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)" , err, i, v.Name))
 			return err
 		}
 	}
