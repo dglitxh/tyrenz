6926177e5e3e16a2b7abe287bbe1ed8125e0e43a
ydeezly <ydglitch@gmail.com>
2023-08-09 18:15:10 +0000

    added timeout to execution

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -1,9 +1,11 @@
 package goblin
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/dglitxh/tyrenz/helpers"
 )
@@ -33,15 +35,22 @@ func (p *Process) NewStep (s Step) Step {
 }
 
 func (s Step) Execute () error{
-	task := exec.Command(s.Cmd, s.Args...)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	task := exec.CommandContext(ctx, s.Cmd, s.Args...)
 	task.Dir = s.Dir
 	if err := task.Run(); err != nil {
+		if ctx.Err() == context.DeadlineExceeded {
+			helpers.Logger(logfn, fmt.Sprintf("Error: %v, timeout", err))
+			return fmt.Errorf("error: %v, timeout", err)
+		}
 		return err
 	}
 	return nil 
 }
 
 func (p *Process) Run () error {
+	
 	for i, v := range *p {
 		if err := v.Execute(); err != nil {
 			helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)" , err, i, v.Name))
