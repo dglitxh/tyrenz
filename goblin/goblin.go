87a1bc87bed85626c259062a89133deb6aec4f24
ydeezly <ydglitch@gmail.com>
2023-08-15 22:17:16 +0000

    fix: run func

--- goblin/goblin.go
+++ goblin/goblin.go
@@ -13,28 +13,28 @@ import (
 var logfn *os.File = helpers.CreateLogFile(".goblogs")
 
 type Step struct {
-	Name string `json:"name"`
-	Dir string	`json:"dir"`
-	Cmd string	`json:"cmd"`
-	Msg string  `json:"msg"`
+	Name string   `json:"name"`
+	Dir  string   `json:"dir"`
+	Cmd  string   `json:"cmd"`
+	Msg  string   `json:"msg"`
 	Args []string `json:"args"`
 }
 
 type Process []Step
 
-func (p *Process) NewStep (s Step) Step {
+func (p *Process) NewStep(s Step) Step {
 	step := Step{
 		Name: s.Name,
-		Dir: s.Dir,
-		Msg: s.Msg,
-		Cmd: s.Cmd,
+		Dir:  s.Dir,
+		Msg:  s.Msg,
+		Cmd:  s.Cmd,
 		Args: s.Args,
 	}
 	*p = append(*p, step)
 	return step
 }
 
-func (s Step) TimeOutExecute () error{
+func (s Step) TimeOutExecute() error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
 	defer cancel()
 	task := exec.CommandContext(ctx, s.Cmd, s.Args...)
@@ -46,33 +46,33 @@ func (s Step) TimeOutExecute () error{
 		}
 		return err
 	}
-	return nil 
+	return nil
 }
 
-func (s Step) Execute () error{
+func (s Step) Execute() error {
 	task := exec.Command(s.Cmd, s.Args...)
 	task.Dir = s.Dir
 	if err := task.Run(); err != nil {
 		return err
 	}
-	return nil 
+	return nil
 }
 
-func (p *Process) Run (timeout bool) error {
-	
+func (p *Process) Run(timeout bool) error {
+
 	for i, v := range *p {
 		if !timeout {
 			if err := v.Execute(); err != nil {
-				helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)" , err, i, v.Name))
+				helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)", err, i, v.Name))
 				return err
+			}
 		} else {
 			if err := v.TimeOutExecute(); err != nil {
-				helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)" , err, i, v.Name))
+				helpers.Logger(logfn, fmt.Sprintf("Error: %v at step %d (%s)", err, i, v.Name))
 				return err
+			}
 		}
-		}
-		}
-	
+
 	}
-	return nil 
+	return nil
 }
